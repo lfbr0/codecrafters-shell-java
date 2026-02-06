0cde9416f6a6f94d7d3226dadb84116f5c572a87
Luís Filipe Ribeiro <20361894+lfbr0@users.noreply.github.com>
2026-02-06 22:58:22 +0000

    codecrafters submit [skip ci]

--- src/main/java/command/PwdCommand.java
+++ src/main/java/command/PwdCommand.java
@@ -8,7 +8,15 @@ import java.net.URI;
 public class PwdCommand implements CodeCraftersShellCommand {
     @Override
     public void execute(OutputStream outputStream, OutputStream errorStream, String... args) throws Exception {
-        URI uri = URI.create(new File(".").getAbsolutePath()).normalize();
-        new PrintStream(outputStream).println(uri);
+        String pwd = URI.create(new File(".").getAbsolutePath())
+                .normalize()
+                .toString();
+
+        // trim trailing "/"
+        if (pwd.endsWith(File.separator)) {
+            pwd = pwd.substring(0, pwd.length()-1);
+        }
+
+        new PrintStream(outputStream).println(pwd);
     }
 }
