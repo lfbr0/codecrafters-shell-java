38f84bc898018f46c1b141fb9baf595194a4dd1a
Luís Filipe Ribeiro <20361894+lfbr0@users.noreply.github.com>
2026-02-07 22:02:49 +0000

    codecrafters submit [skip ci]

--- src/main/java/CodeCraftersShell.java
+++ src/main/java/CodeCraftersShell.java
@@ -3,7 +3,6 @@ import environment.CodeCraftersShellEnvironment;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.reader.impl.DefaultParser;
-import org.jline.reader.impl.LineReaderImpl;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
@@ -11,6 +10,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CodeCraftersShell implements AutoCloseable {
 
@@ -36,7 +40,10 @@ public class CodeCraftersShell implements AutoCloseable {
         errorStream.close();
     }
 
-    // perform REPL cycle in shell
+    /**
+     * Starts the shell REPL
+     * @throws IOException
+     */
     public void repl() throws IOException {
         DefaultParser parser = new DefaultParser();
         parser.setEscapeChars(new char[0]);
@@ -76,10 +83,14 @@ public class CodeCraftersShell implements AutoCloseable {
         }
     }
 
+    /**
+     * Interpret a line of input from the user and execute it
+     * @param line line of input from user
+     */
     private void interpret(String line) {
         // get command and args
         String command = line.split(" ")[0].trim();
-        String[] args = line.substring(command.length()).trim().split(" ");
+        String[] args = parseArguments(line.substring(command.length()).trim());
 
         // if exit condition, then exit shell
         if (command.equals("exit")) {
@@ -115,4 +126,70 @@ public class CodeCraftersShell implements AutoCloseable {
         }
     }
 
+    /**
+     * From the input line of the user, parse out the arguments
+     * @param argsLine raw input line of user
+     * @return array of arguments
+     */
+    private String[] parseArguments(String argsLine) {
+        List<String> argsArray = new LinkedList<>();
+        String currentArg = "";
+
+        boolean inSingleQuotes = false;
+        boolean inDoubleQuotes = false;
+
+        for (int i = 0; i < argsLine.length(); i++) {
+            char c = argsLine.charAt(i);
+            boolean commitArg = false;
+
+            if (c == '\'' && !inDoubleQuotes) {
+                inSingleQuotes = !inSingleQuotes;
+                continue;
+            }
+
+            if (c == '"' && !inSingleQuotes) {
+                inDoubleQuotes = !inDoubleQuotes;
+                continue;
+            }
+
+            if (Character.isWhitespace(c) && !inSingleQuotes && !inDoubleQuotes) {
+                commitArg = true;
+            }
+
+            // I understand this is not performant since strings are immutable, but I don't give a fuck.
+            currentArg += c;
+
+            // if we should commit this, or we're at the end of the line, add it to args
+            if (commitArg) {
+                // do not append if empty
+                String currentArgStr = currentArg.trim();
+
+                // remove quotes from arg
+                if ( (currentArgStr.startsWith("\"") && currentArgStr.endsWith("\"")) ||
+                        (currentArgStr.startsWith("'") && currentArgStr.endsWith("'")) ) {
+                    currentArgStr = currentArgStr.substring(1, currentArgStr.length() - 1);
+                }
+
+                if (!currentArgStr.isEmpty()) {
+                    argsArray.add(currentArgStr);
+                }
+                currentArg = "";
+            }
+        }
+
+        if (!currentArg.isEmpty()) {
+            String currentArgStr = currentArg.trim();
+
+            if ( (currentArgStr.startsWith("\"") && currentArgStr.endsWith("\"")) ||
+                    (currentArgStr.startsWith("'") && currentArgStr.endsWith("'")) ) {
+                currentArgStr = currentArgStr.substring(1, currentArgStr.length() - 1);
+            }
+
+            if (!currentArgStr.isEmpty()) {
+                argsArray.add(currentArgStr);
+            }
+        }
+
+        return argsArray.toArray(new String[0]);
+    }
 }
