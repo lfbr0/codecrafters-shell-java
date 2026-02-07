6a04fc73834b647aec39444e0ac57c9967bd9b75
Luís Filipe Ribeiro <20361894+lfbr0@users.noreply.github.com>
2026-02-07 14:31:18 +0000

    codecrafters submit [skip ci]

--- src/main/java/command/HistoryCommand.java
+++ src/main/java/command/HistoryCommand.java
@@ -6,7 +6,10 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class HistoryCommand implements CodeCraftersShellCommand {
 
@@ -18,9 +21,20 @@ public class HistoryCommand implements CodeCraftersShellCommand {
 
     @Override
     public void execute(OutputStream outputStream, OutputStream errorStream, String... args) throws Exception {
-        PrintStream printStream = new PrintStream(outputStream);
-        for (int i = 0; i < shellEnvironment.getHistory().size(); i++) {
-            printStream.printf("\t%d %s\n", i+1, shellEnvironment.getHistory().get(i));
+        List<String> history = shellEnvironment.getHistory();
+
+        Stream<String> historyStream = IntStream
+                .range(0, history.size()) // enumerate from 0 .. history.length
+                .mapToObj(i -> String.format("\t%d %s\n", i+1, history.get(i)));
+
+        // if there is arguments, and first is number of history lines
+        if (args != null && args.length >= 1 && args[0].matches("[0-9]+")) {
+            int limit = Integer.parseInt(args[0]);
+            historyStream = historyStream.limit(limit);
         }
+
+        // for each, print them
+        PrintStream printStream = new PrintStream(outputStream);
+        historyStream.forEach(printStream::print);
     }
 }
